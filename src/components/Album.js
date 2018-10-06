2e36d3e4e9973c1ab41d58a5b837db88fd11c2cd
Trevor <trevorgillette17@gmail.com>
2018-10-05 17:43:46 -0700

    Components:Album Assignment Work

--- src/components/Album.js
+++ src/components/Album.js
@@ -5,10 +5,11 @@ class Album extends Component {
 	constructor(props) {
 		super(props);
 	
+
 	const album = albumData.find( album => {
 		return album.slug === this.props.match.params.slug
 	});
-
+		
 	this.state = {
 		album: album
 	};
@@ -17,11 +18,11 @@ class Album extends Component {
 		return (
 			<section className="album">
 			<section id="album-info">
-			<img id="album-cover-art" src={this.state.album.albumcover} alt={this.state.album.title} />
+			<img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
 			<div className="album-details">
-			<h1 id="album-title">this.state.album.title</h1>
-			<h2 className="artist">this.state.album.artist</h2>
-			<div id="release-info">this.state.album.releaseInfo</div>
+			<h1 id="album-title">{this.state.album.title}</h1>
+			<h2 className="artist">{this.state.album.artist}</h2>
+			<div id="release-info">{this.state.album.releaseInfo}</div>
 			</div>
 			</section>
 			<table id="song-list">
@@ -31,8 +32,15 @@ class Album extends Component {
 			<col id="song-duration-column" />
 			</colgroup>
 			<tbody>
+			{this.state.album.songs.map( (song, index) =>( 
+				<tr key={index}>
+				<td>{index+1}</td>
+				<td>{song.title}</td>
+				<td>{song.duration}</td>
+				</tr>
+			))}
 			</tbody>
-			</table>
+				</table>
 			</section>
 		);
 	}
